36336df4567df723160775ef11ad7a4471459c50
Konstantin-Kiselov <constantine.kiss@gmail.com>
2021-10-25 02:42:37 +0300

    add useEffect for localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,20 +15,18 @@ export default function App() {
   ]);
   const [filter, setFilter] = useState('');
 
-  // useEffect(() => {
-  //   const contacts = localStorage.getItem('contacts');
-  //   const parsedContacts = JSON.parse(contacts);
+  useEffect(() => {
+    const contactsLocalStor = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contactsLocalStor);
 
-  //   if (parsedContacts) {
-  //     setContacts(parsedContacts);
-  //   }
-  // }, [contacts]);
+    if (parsedContacts) {
+      setContacts(parsedContacts);
+    }
+  }, []);
 
-  // componentDidUpdate(prevState) {
-  //   if (this.state.contacts !== prevState.contacts) {
-  //     localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-  //   }
-  // }
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
   const getVisibleContacts = () => {
     const normalizedFilter = filter.toLowerCase();
@@ -55,9 +53,9 @@ export default function App() {
   };
 
   const deleteContact = contactId => {
-    setContacts(contacts => {
-      return contacts.filter(contact => contact.id !== contactId);
-    });
+    setContacts(contacts =>
+      contacts.filter(contact => contact.id !== contactId),
+    );
   };
 
   const changeFitler = e => {
@@ -81,88 +79,3 @@ export default function App() {
     </Container>
   );
 }
-
-// class App extends Component {
-//   state = {
-//     contacts: [
-//       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-//     ],
-//     filter: '',
-//   };
-
-// componentDidMount() {
-// const contacts = localStorage.getItem('contacts');
-// const parsedContacts = JSON.parse(contacts);
-
-// if (parsedContacts) {
-//   this.setState({ contacts: parsedContacts });
-// }
-// }
-
-// componentDidUpdate(prevState) {
-//   if (this.state.contacts !== prevState.contacts) {
-//     localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-//   }
-// }
-
-// addContact = ({ name, number }) => {
-//   const contact = {
-//     id: uuidv4(),
-//     name,
-//     number,
-//   };
-
-//   this.setState(({ contacts }) => {
-//     if (contacts.some(contact => contact.name === name)) {
-//       return alert(`${name} is already in contacts!`);
-//     }
-//     return {
-//       contacts: [contact, ...contacts],
-//     };
-//   });
-// };
-
-// deleteContact = contactId => {
-//   this.setState(prevState => ({
-//     contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-//   }));
-// };
-
-// changeFitler = e => {
-//   this.setState({ filter: e.target.value });
-// };
-
-//   getVisibleContacts = () => {
-//     const { contacts, filter } = this.state;
-//     const normalizedFilter = filter.toLowerCase();
-
-//     return contacts.filter(contact =>
-//       contact.name.toLowerCase().includes(normalizedFilter),
-//     );
-//   };
-
-//   render() {
-//     const { filter } = this.state;
-//     const visibleContacts = this.getVisibleContacts();
-
-//     return (
-//       <Container>
-//         <Section title="Phonebook">
-//           <Form onSubmit={this.addContact} />
-//         </Section>
-//         <Section title="Contacts">
-//           <Filter value={filter} onChange={this.changeFitler} />
-//           <ContactList
-//             contacts={visibleContacts}
-//             onDeleteContact={this.deleteContact}
-//           />
-//         </Section>
-//       </Container>
-//     );
-//   }
-// }
-
-// export default App;
